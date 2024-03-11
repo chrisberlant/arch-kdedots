5b73d9efc141b8192497e04c1337681b1e0ae837
Chris Berlant <chris.berlant@gmail.com>
2024-03-12 00:36:52 +0100

    updated kitty config and neofetch images

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Apps to install
-apps=("neofetch" "visual-studio-code-bin" "chromium" "npm" "nodejs" "kitty" "zsh" "oh-my-zsh-git" "zsh-theme-powerlevel10k" "pokemon-colorscripts-git" "eza")
+apps=("neofetch" "visual-studio-code-bin" "chromium" "npm" "nodejs" "kitty" "zsh" "oh-my-zsh-git" "zsh-theme-powerlevel10k" "pokemon-colorscripts-git" "eza" "onlyoffice-bin")
 
 # Function to check if a package is installed
 package_installed() {
     pacman -Q "$1" &>/dev/null
 }
 
-# Loop over every apps
+# Loop on the apps array
 for app in "${apps[@]}"; do
     if ! package_installed $app; then
         echo "Installing $app..."
@@ -26,3 +26,4 @@ cp -r ./fonts ~/.local/share/
 cp -r ./home/. ~/
 cp -r ./config/. ~/.config
 ln -s ~/.config/kitty/themes/Tokyo-Night.conf ~/.config/kitty/theme.conf
+echo "Configuration finished."
