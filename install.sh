ae14a40c37f7b3265df104d4b4c3d98aaa5ec63d
Chris Berlant <chris.berlant@gmail.com>
2024-10-27 23:53:35 +0100

    refactor: added install_package function

--- install.sh
+++ install.sh
@@ -4,12 +4,22 @@
 apps=("neofetch" "visual-studio-code-bin" "chromium" "npm" "nodejs" "kitty" "zsh" "oh-my-zsh-git" "zsh-theme-powerlevel10k" "eza" "onlyoffice-bin")
 
 # Function to check if a package is installed
-package_installed() {
+package_exists() {
     pacman -Q "$1" &>/dev/null
 }
 
+# Function to install a package
+install_package() {
+    if ! package_exists "$1"; then
+        echo "Installing $1..."
+        yay -S --noconfirm "$1" || { echo "Failed to install $1."; }
+    else
+        echo "$1 is already installed."
+    fi
+}
+
 # Check if git is installed
-if ! package_installed 'git'; then
+if ! package_exists 'git'; then
     echo "Installing git..."
     sudo pacman -S --noconfirm git || { echo "Failed to install git."; }
 else
@@ -17,12 +27,13 @@ else
 fi
 
 # Check if yay is installed
-if ! package_installed 'yay'; then
+if ! package_exists 'yay'; then
     echo "Installing yay from AUR..."
-    sudo pacman -S --needed --noconfirm base-devel || { echo "Failed to install base-devel."; }
+    sudo pacman -S --needed --noconfirm base-devel || { echo "Failed to install base-devel."; exit 1; }
     git clone https://aur.archlinux.org/yay.git || { echo "Failed to clone yay."; exit 1; }
-    cd yay && makepkg -si --noconfirm || { echo "Failed to build yay."; }
-    cd .. && rm -rf yay || { echo "Failed to clean up yay directory."; }
+    cd yay || { echo "Failed to enter yay directory."; exit 1; }
+    makepkg -si --noconfirm || { echo "Failed to build yay."; exit 1; }
+    cd .. && rm -rf yay
     echo "Yay installation completed."
 else
     echo "Yay is already installed."
@@ -30,26 +41,20 @@ fi
 
 # Loop on the apps array
 for app in "${apps[@]}"; do
-    if ! package_installed $app; then
-        echo "Installing $app..."
-        yay -S --noconfirm $app || { echo "Failed to install $app."; }
-    else
-        echo "$app is already installed."
-    fi
+    install_package "$app"
 done
 
 # Copy config files
 echo "Creating config files..."
+mkdir -p ~/.local/share/fonts
+mkdir -p ~/.config
 cp -r ./fonts ~/.local/share/ || { echo "Failed to copy fonts."; }
 cp -r ./home/. ~/ || { echo "Failed to copy home files."; }
 cp -r ./config/. ~/.config || { echo "Failed to copy config files."; }
 
 # Ensure kitty theme directory exists before linking
-if [[ -d ~/.config/kitty/themes ]]; then
-    ln -sf ~/.config/kitty/themes/Tokyo-Night.conf ~/.config/kitty/theme.conf || { echo "Failed to create symlink for kitty theme."; }
-else
-    echo "Kitty themes directory not found, skipping theme configuration."
-fi
+mkdir -p ~/.config/kitty/themes
+ln -sf ~/.config/kitty/themes/Tokyo-Night.conf ~/.config/kitty/theme.conf || { echo "Failed to create symlink for kitty theme."; }
 
 # Change default shell to zsh
 if [[ "$(echo $SHELL)" != "/usr/bin/zsh" ]]; then
